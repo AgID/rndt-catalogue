37a8ca5f208df36874447e11f131faaab3d9826d
Antonio Rotundo <anto.rotundo@gmail.com>
2022-06-08 22:41:42 +0200

    Add files via upload

--- DB/Database.sql
+++ DB/Database.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `geoportal` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `geoportal`;
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -12,7 +11,6 @@ USE `geoportal`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 --
 -- Table structure for table `gpt_collection`
@@ -56,9 +54,9 @@ CREATE TABLE `gpt_harvesting_history` (
   `UUID` varchar(38) NOT NULL,
   `HARVEST_ID` varchar(38) NOT NULL,
   `HARVEST_DATE` datetime DEFAULT NULL,
-  `HARVESTED_COUNT` int(11) DEFAULT '0',
-  `VALIDATED_COUNT` int(11) DEFAULT '0',
-  `PUBLISHED_COUNT` int(11) DEFAULT '0',
+  `HARVESTED_COUNT` int DEFAULT '0',
+  `VALIDATED_COUNT` int DEFAULT '0',
+  `PUBLISHED_COUNT` int DEFAULT '0',
   `HARVEST_REPORT` longtext,
   PRIMARY KEY (`UUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -115,18 +113,18 @@ DROP TABLE IF EXISTS `gpt_pa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_pa` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID` int NOT NULL AUTO_INCREMENT,
   `cod_ipa` varchar(45) DEFAULT NULL,
   `nome` varchar(255) DEFAULT NULL,
-  `ultimoID1` int(11) DEFAULT NULL,
-  `ultimoID2` int(11) DEFAULT NULL,
+  `ultimoID1` int DEFAULT NULL,
+  `ultimoID2` int DEFAULT NULL,
   `tipoPA` varchar(255) DEFAULT NULL,
   `codiceTipoPA` varchar(45) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID_UNIQUE` (`ID`),
   UNIQUE KEY `cod_ipa_UNIQUE` (`cod_ipa`),
   UNIQUE KEY `nome_UNIQUE` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=2055 DEFAULT CHARSET=utf8 COMMENT='Elenco delle pubbliche amministrazioni legate all''utente';
+) ENGINE=InnoDB AUTO_INCREMENT=2115 DEFAULT CHARSET=utf8 COMMENT='Elenco delle pubbliche amministrazioni legate all''utente';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,10 +137,10 @@ DROP TABLE IF EXISTS `gpt_resource`;
 CREATE TABLE `gpt_resource` (
   `DOCUUID` varchar(38) NOT NULL,
   `TITLE` varchar(4000) DEFAULT NULL,
-  `OWNER` int(11) NOT NULL,
+  `OWNER` int NOT NULL,
   `INPUTDATE` datetime DEFAULT NULL,
   `UPDATEDATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `ID` int(32) NOT NULL AUTO_INCREMENT,
+  `ID` int NOT NULL AUTO_INCREMENT,
   `APPROVALSTATUS` varchar(64) DEFAULT NULL,
   `PUBMETHOD` varchar(64) DEFAULT NULL,
   `SITEUUID` varchar(38) DEFAULT NULL,
@@ -158,6 +156,7 @@ CREATE TABLE `gpt_resource` (
   `SEARCHABLE` varchar(6) DEFAULT NULL,
   `SYNCHRONIZABLE` varchar(6) DEFAULT NULL,
   `LASTSYNCDATE` datetime DEFAULT NULL,
+  `FILTRO_CSW` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`DOCUUID`),
   UNIQUE KEY `ID` (`ID`),
   KEY `GPT_RESOURCE_IDX1` (`SITEUUID`),
@@ -171,7 +170,7 @@ CREATE TABLE `gpt_resource` (
   KEY `GPT_RESOURCE_IDX11` (`ACL`(255)),
   KEY `GPT_RESOURCE_IDX12` (`PROTOCOL_TYPE`),
   KEY `GPT_RESOURCE_IDX13` (`LASTSYNCDATE`)
-) ENGINE=InnoDB AUTO_INCREMENT=80419 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=126798 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -183,7 +182,25 @@ DROP TABLE IF EXISTS `gpt_resource_data`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_resource_data` (
   `DOCUUID` varchar(38) NOT NULL,
-  `ID` int(32) NOT NULL,
+  `ID` int NOT NULL,
+  `XML` longtext,
+  `THUMBNAIL` longblob,
+  PRIMARY KEY (`DOCUUID`),
+  UNIQUE KEY `ID` (`ID`),
+  KEY `GPT_RESOURCE_DATA_IDX1` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gpt_resource_data_backup`
+--
+
+DROP TABLE IF EXISTS `gpt_resource_data_backup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gpt_resource_data_backup` (
+  `DOCUUID` varchar(38) NOT NULL,
+  `ID` int NOT NULL,
   `XML` longtext,
   `THUMBNAIL` longblob,
   PRIMARY KEY (`DOCUUID`),
@@ -201,15 +218,10 @@ DROP TABLE IF EXISTS `gpt_resource_stat`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_resource_stat` (
   `DOCUUID` varchar(38) NOT NULL,
-  `ID` int(32) NOT NULL,
+  `ID` int NOT NULL,
   `HIERARCHY` varchar(255) DEFAULT NULL,
-  `RESPONSIBLE` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`DOCUUID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
-  KEY `IDX_HIER` (`HIERARCHY`),
-  KEY `IDX_DOC` (`DOCUUID`),
-  KEY `IDX_RESP` (`RESPONSIBLE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella perla gestione dei dati statistici';
+  `RESPONSIBLE` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -221,13 +233,9 @@ DROP TABLE IF EXISTS `gpt_resource_stat_inspire_service`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_resource_stat_inspire_service` (
   `DOCUUID` varchar(38) NOT NULL,
-  `ID` int(32) NOT NULL,
-  `INSPIRE_SERVICE` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
-  KEY `IDX_DOC` (`DOCUUID`),
-  KEY `IDX_SERVICE` (`INSPIRE_SERVICE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella perla gestione dei dati statistici Inspire Service Keyword';
+  `ID` int NOT NULL,
+  `INSPIRE_SERVICE` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -239,13 +247,43 @@ DROP TABLE IF EXISTS `gpt_resource_stat_inspire_theme`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_resource_stat_inspire_theme` (
   `DOCUUID` varchar(38) NOT NULL,
-  `ID` int(32) NOT NULL,
-  `INSPIRE_THEME` varchar(255) DEFAULT NULL,
+  `ID` int NOT NULL,
+  `INSPIRE_THEME` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gpt_resource_stat_opendata`
+--
+
+DROP TABLE IF EXISTS `gpt_resource_stat_opendata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gpt_resource_stat_opendata` (
+  `DOCUUID` varchar(38) NOT NULL,
+  `ID` int NOT NULL,
+  `OPENDATA` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
   KEY `IDX_DOC` (`DOCUUID`),
-  KEY `IDX_THEME` (`INSPIRE_THEME`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella perla gestione dei dati statistici Inspire theme Keyword';
+  KEY `IDX_OPEN` (`OPENDATA`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella per la gestione dei dati statistici Opendata';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gpt_resource_stat_priority_dataset`
+--
+
+DROP TABLE IF EXISTS `gpt_resource_stat_priority_dataset`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gpt_resource_stat_priority_dataset` (
+  `DOCUUID` varchar(38) NOT NULL,
+  `ID` int NOT NULL,
+  `PRIORITYDATASET` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `IDX_DOC` (`DOCUUID`),
+  KEY `IDX_PRIORITY` (`PRIORITYDATASET`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella per la gestione dei dati statistici Priority Dataset';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -257,13 +295,9 @@ DROP TABLE IF EXISTS `gpt_resource_stat_service_type`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_resource_stat_service_type` (
   `DOCUUID` varchar(38) NOT NULL,
-  `ID` int(32) NOT NULL,
-  `SERVICE_TYPE` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
-  KEY `IDX_DOC` (`DOCUUID`),
-  KEY `IDX_SERVICE` (`SERVICE_TYPE`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella perla gestione dei dati statistici Service type keyword';
+  `ID` int NOT NULL,
+  `SERVICE_TYPE` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -275,13 +309,9 @@ DROP TABLE IF EXISTS `gpt_resource_stat_topic`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_resource_stat_topic` (
   `DOCUUID` varchar(38) NOT NULL,
-  `ID` int(32) NOT NULL,
-  `TOPIC` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID_UNIQUE` (`ID`),
-  KEY `IDX_DOC` (`DOCUUID`),
-  KEY `IDX_TOPIC` (`TOPIC`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tabella perla gestione dei dati statistici Topic Keyword';
+  `ID` int NOT NULL,
+  `TOPIC` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -294,7 +324,7 @@ DROP TABLE IF EXISTS `gpt_search`;
 CREATE TABLE `gpt_search` (
   `UUID` varchar(38) NOT NULL,
   `NAME` varchar(255) DEFAULT NULL,
-  `USERID` int(32) DEFAULT NULL,
+  `USERID` int DEFAULT NULL,
   `CRITERIA` longtext,
   PRIMARY KEY (`UUID`),
   KEY `GPT_SEARCH_IDX1` (`USERID`)
@@ -325,16 +355,16 @@ DROP TABLE IF EXISTS `gpt_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_user` (
-  `USERID` int(32) NOT NULL AUTO_INCREMENT,
+  `USERID` int NOT NULL AUTO_INCREMENT,
   `DN` varchar(900) DEFAULT NULL,
   `USERNAME` varchar(64) DEFAULT NULL,
-  `FK_IDPA` int(11) DEFAULT NULL,
+  `FK_IDPA` int DEFAULT NULL,
   PRIMARY KEY (`USERID`),
   UNIQUE KEY `DN_UNIQUE` (`DN`),
   UNIQUE KEY `USERNAME_UNIQUE` (`USERNAME`),
   KEY `GPT_USER_IDX1` (`DN`(255)),
   KEY `GPT_USER_IDX2` (`USERNAME`)
-) ENGINE=InnoDB AUTO_INCREMENT=1057 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1149 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -345,23 +375,12 @@ DROP TABLE IF EXISTS `gpt_user_login`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gpt_user_login` (
-  `ID` int(32) NOT NULL AUTO_INCREMENT,
-  `USERID` int(32) DEFAULT NULL,
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `USERID` int DEFAULT NULL,
   `DATA_IN` datetime DEFAULT NULL,
   `DATA_OUT` datetime DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `GPT_USER_IDX2` (`USERID`)
-) ENGINE=InnoDB AUTO_INCREMENT=808 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5965 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed
